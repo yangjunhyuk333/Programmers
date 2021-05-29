7efcc24574f2e1368fb97ef57a278986de06b0fa
yangjunhyuk333 <chuchoun1234@gmail.com>
2021-05-29 14:47:06 +0900

    [ADD]search Solution

---
+++ src/search/Main.java
@@ -0,0 +1,9 @@
+package search;
+
+public class Main {
+    public static void main(String[] args) {
+        String numbers = "17";
+        Solution solution = new Solution();
+        System.out.println(solution.solution(numbers));
+    }
+}
---
+++ src/search/Solution.java
@@ -0,0 +1,13 @@
+package search;
+
+import java.util.*;
+
+public class Solution {
+    public int solution(String numbers){
+        int answer = 0;
+
+
+
+        return answer;
+    }
+}
